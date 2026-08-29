bbbafd24634065dc6ba9157dcea689993b686699
Nitish5196 <nitishkumar18082010@gmail.com>
2026-08-29 19:02:55 +0530

    debugging and deadscreen and winscreen added

--- Scenes/death_screen.gd
+++ Scenes/death_screen.gd
@@ -1,12 +1,12 @@
 extends Control
 
-
 func _on_retry_pressed() -> void:
-	# Reset the game back to its starting state
+	# Reset game state
 	Global.lives = 3
 	Global.minigames_done = 0
-
+	
+	# UNPAUSE THE TREE!
+	get_tree().paused = false
+	
 	# Start the game again
-	get_tree().change_scene_to_file(
-		"res://Scenes/timer_screen.tscn"
-	)
+	get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
--- minigame_2.gd
+++ minigame_2.gd
@@ -13,11 +13,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if buttons_pressed == 4:
 		if Global.minigames_done > 3:
-			get_tree().change_scene_to_file("res://scenes/done_screen.tscn")
+			get_tree().change_scene_to_file("res://Scenes/win_screen.tscn")
 		else:
-			get_tree().change_scene_to_file("res://scenes/timer_screen.tscn")
+			get_tree().change_scene_to_file("res://Scenes/timer_screen.tscn")
 	
 	if timer_end:
 		Global.lives -= 1
 		Global.minigames_done -=1
-		get_tree().change_scene_to_file("res://scenes/timer_screen.tscn")
+		get_tree().change_scene_to_file("res://Scenes/timer_screen.tscn")
